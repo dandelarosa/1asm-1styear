64abeccaf860d7dc27d895a0f2017b1bd8ae535d
Dan Dela Rosa <dandelarosadeveloper@gmail.com>
2018-04-08 20:05:43 -0400

    Add placeholder character

--- js/main.js
+++ js/main.js
@@ -2,19 +2,27 @@ var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 480,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false
+    }
+  },
   scene: {
-      preload: preload,
-      create: create,
-      update: update
+    preload: preload,
+    create: create,
+    update: update
   }
 };
 
 var game = new Phaser.Game(config);
 
 function preload() {
+  this.load.image('box32', 'assets/box32.png');
 }
 
 function create() {
+  player = this.physics.add.sprite(150, 200, 'box32');
 }
 
 function update() {
