a1f04245930d5f34727397bf9181671cf575fb70
Dan Dela Rosa <dandelarosadeveloper@gmail.com>
2018-04-22 17:51:28 -0400

    Implement collisions with level grid

--- js/main.js
+++ js/main.js
@@ -23,7 +23,16 @@ function create() {
   var tiles = map.addTilesetImage('tileset', 'tiles');
   var layer = map.createStaticLayer(0, tiles, 0, 0);
 
+  var rawTileArray = this.cache.tilemap.get('level1').data.layers[0].data;
+  var numCols = this.cache.tilemap.get('level1').data.width;
+  this.grid = new Grid2D(rawTileArray, numCols);
+  this.gridCollider = new GridCollider(this.grid);
+  
   this.player = this.add.sprite(150, 200, 'box32');
+  // MUST initialize values (or first frame's physics will break)
+  this.player.dx = 0;
+  this.player.dy = 0;
+  this.player.setOrigin(0, 0);
 
   this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
   this.cameras.main.startFollow(this.player);
@@ -31,12 +40,30 @@ function create() {
 
 function update() {
   cursors = this.input.keyboard.createCursorKeys();
+
   if (cursors.left.isDown)
   {
-    this.player.x -= 4;
+    this.player.dx = -4;
   }
   else if (cursors.right.isDown)
   {
-    this.player.x += 4;
+    this.player.dx = 4;
   }
+  else if (cursors.up.isDown)
+  {
+    this.player.dy = -4;
+  }
+  else if (cursors.down.isDown)
+  {
+    this.player.dy = 4;
+  }
+
+  this.gridCollider.handleCollisionsWith(this.player);
+
+  this.player.x += this.player.dx;
+  this.player.y += this.player.dy;
+
+  // Reset velocity for now
+  this.player.dx = 0;
+  this.player.dy = 0;
 }
